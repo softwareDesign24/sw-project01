448505e169ad33425c5479a67b00d8bb12959ef3
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2024-02-04 23:27:53 +0200

    Update Calculator.cpp

--- 3. Implementation/Calculator.cpp
+++ 3. Implementation/Calculator.cpp
@@ -1,27 +1,13 @@
-/**
- * Project Untitled
- */
-
-
 #include "Calculator.h"
 
-/**
- * Calculator implementation
- */
-
-
 void Calculator::add() {
-
 }
 
 void Calculator::substract() {
-
 }
 
 void Calculator::multiply() {
-
 }
 
 void Calculator::divide() {
-
 }
