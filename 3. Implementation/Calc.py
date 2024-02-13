94f14e8b580e4fbd7a987497e65e09fd33fa59e5
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2024-02-13 23:16:57 +0200

    update

--- 3. Implementation/Calc.py
+++ 3. Implementation/Calc.py
@@ -4,13 +4,13 @@ class Calculator:
     self.var02 = var02
 
   def plus(self):
-    print(self.var01 + self.var02)
+    return self.var01 + self.var02
 
   def minus(self):
-    print(self.var01 - self.var02)
+    return self.var01 - self.var02
 
   def multiply(self):
-    print(self.var01 * self.var02)
+    return self.var01 * self.var02
 
   def divide(self):
-    print(self.var01 / self.var02)
+    return self.var01 / self.var02
