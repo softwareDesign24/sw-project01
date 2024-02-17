806e35d02ee246c3f451b5d77d48a0114f57f9f9
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2024-02-18 01:53:45 +0200

    ...

--- 3. Implementation/test_a01.py
+++ 3. Implementation/test_a01.py
@@ -1,13 +1,13 @@
 import pytest
 from Calc import Calculator
 
-# test_plus(1,5,6)
-def test01():
+# ...
+def test_01():
     a = Calculator(1,1)
     """ Check that plus(1,1) = 2 """
     assert a.plus() == 2
 
-def test02():
+def test_02():
     a = Calculator(5,10)
     """ Check that plus(5,10) = 15 """
     assert a.plus() == 15
