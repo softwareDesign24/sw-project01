35ce1e3f553bf295400991acfa0041a324fec1f0
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2024-02-18 00:45:02 +0200

    convert unittest to pytest

--- 3. Implementation/test01.py
+++ 3. Implementation/test01.py
@@ -1,18 +1,16 @@
-import unittest
+import pytest
 from Calc import Calculator
 
 # test_plus(1,5,6)
+def test01():
+    a = Calculator(1,1)
+    """ Check that plus(1,1) = 2 """
+    assert a.plus() == 2
 
-class Tests(unittest.TestCase):
-    def test01(self):
-        a = Calculator(1,1)
-        """ Check that plus(1,1) = 2 """
-        self.assertTrue(a.plus() == 2)
-
-    def test02(self):
-        a = Calculator(5,10)
-        """ Check that plus(5,10) = 15 """
-        self.assertTrue(a.plus() == 15)
+def test02():
+    a = Calculator(5,10)
+    """ Check that plus(5,10) = 15 """
+    assert a.plus() == 15
 
 if __name__ == "__main__":
     unittest.main()
